c1c3e20888c99b74b4639eef4e0032d2babbcf7b
Sergio Cabral <scastrocabral@gmail.com>
2022-02-14 15:38:41 -0300

    Encapsulation

---
+++ src/ContaPoupanca.java
@@ -0,0 +1,33 @@
+public class ContaPoupanca {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
